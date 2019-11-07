7cfa6a9ace26da53ab053f92b6e3018d31bfe88a
wobupile <39699271+wobupile@users.noreply.github.com>
2019-11-07 21:01:23 +0800

    Add files via upload

---
+++ build/precache-manifest.c12715862136c00d15472565262c4b3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3e9d120b4ba8bc0d5fca1102bdf1976",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1b77ba34c14d47ac93a",
+    "url": "/static/css/main.3788b031.chunk.css"
+  },
+  {
+    "revision": "a89ed1e7874d269fbf00",
+    "url": "/static/js/2.16b5673f.chunk.js"
+  },
+  {
+    "revision": "d1b77ba34c14d47ac93a",
+    "url": "/static/js/main.59ab318b.chunk.js"
+  },
+  {
+    "revision": "83d97cf838c74a71b465",
+    "url": "/static/js/runtime-main.ab3ec558.js"
+  }
+]);
