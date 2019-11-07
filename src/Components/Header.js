5b06952f6730f1c769365f927c3289da3221b923
wobupile <39699271+wobupile@users.noreply.github.com>
2019-11-07 20:50:11 +0800

    Add files via upload

---
+++ src/Components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+
+export default function Header(){
+    return (
+        <div className='headerwrap'>
+            <header>
+                <img src="http://static2.cnodejs.org/public/images/cnodejs_light.svg" alt='header'/>
+                <input type="text" name="search" className="search" placeholder='search'/>
+                <div style={{float:'right',lineHeight:"50px"}}>
+                    <Link to='/home'>首页</Link>
+                    <Link to='/start'>新手入门</Link>
+                    <Link to='/api'>API</Link>
+                    <Link to='/about'>关于</Link>
+                    <Link to='/login'>登录</Link>
+                </div>
+            </header>
+        </div>
+    )
+}
